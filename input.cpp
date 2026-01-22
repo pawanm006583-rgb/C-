dfbd58f863d87296746cc4f3d08b9d6a6397dd50
Pawan Mishra <pawanm006583@gmail.com>
2026-01-22 20:21:48 +0530

    Arrays

---
+++ input.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a,b,c,d,e;
+    cout<<"Enter the first number:";
+    cin>>a;
+    cout<<"Enter the second number:";
+    cin>>b;
+    cout<<"Enter the third number:";
+    cin>>c;
+    cout<<"Enter the 4th number:";
+    cin>>d;
+    cout<<"Enter the 5th number:";
+    cin>>e;
+
+    int array[5]={a,b,c,d,e};
+
+    cout<<"Array elements are:\n";
+    // for(int i=0; i<5;i++){
+    //     cout<<array[i]<<" ";
+    // }
+    return 0;
+
+}
