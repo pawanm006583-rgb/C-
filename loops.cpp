175d00a1cf1c610b2b366697fb6bfcf3d4b0d652
Pawan Mishra <pawanm006583@@gmail.com>
2026-02-08 19:50:33 +0530

    if_else

---
+++ loops.cpp
@@ -0,0 +1,110 @@
+#include <iostream>
+using namespace std;
+
+// int main(){
+//     int i=1;
+//     while (i<=5){
+//         cout<<i<<"";
+//         i++;
+//     }
+//     return 0;
+// }
+
+
+// Even numbers till 50
+int main()
+// {
+//     int i=1;
+//     while (i<=50);
+//     if(i%2==0);{
+//         cout<<i<<"";
+//         i++;
+//     }
+//     return 0;
+// }
+
+
+// Sum of 10 numbers
+// {
+//     int i=1 , sum =0;
+//     while (i<=10){
+//         sum+=i;
+//         i++;
+//     }
+//     cout<<"sum ="<<sum;
+//     return 0;
+    
+// }
+
+
+// Reverse of a number
+// {
+//     int num , reverse =0;
+//     cout<<"Enter a digit";
+//     cin>>num;
+//     while (num>0){
+//     reverse = reverse*10+num%10;
+//     num/=10;
+//     }
+//     cout<<"reverse ="<<reverse;
+// }
+
+
+// {
+//     int n =5;
+//     // cout<<"Enter the number";
+//     for(int i=1; i<=n; i++){
+//         for(int j=1; j<=i; j++){
+//             cout<<"*";
+//         }
+//         cout<<endl;
+//     }
+// }
+
+// {
+//     int n = 5;
+//     for(int i=1; i>=n; i--){
+//         for(int j=1; j<=i; j++){
+//             cout<<"*";
+//         }
+//         cout<<endl;
+//     }
+
+// }
+
+// {
+//     int ch=1;
+//     for(int i=1; i<=5; ++i){
+//     for(int j =i; j<=i; ++j){
+//         cout<<""<<ch;
+//     }
+//     }
+// }
+
+
+// {
+//     int i=10,n;
+//     for(int i=5;i<=n;i++){
+//         for ();
+//     }
+// }
+
+
+// {
+//     int year;
+//     cout<<"Enter a year";
+//     cin>>year;
+//     if((year%4==0)||(year%400==0))
+//     cout<<"Leap year"<<year;
+//     else
+//     cout<<"Not a leap year"<<year;
+// }
+
+{
+    int n;
+    cout<<"Enter the number for the table";
+    cin>>n;
+    for(int i=1;i<=10;i++)
+    cout<<n<<"X"<<i<<"="<<n*i<<endl;
+
+}
