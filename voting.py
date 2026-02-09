62659421431342b5e272dc6bd1cbf7f8d163b13f
Pawan Mishra <pawanm006583@gmail.com>
2026-02-09 20:07:39 +0530

    Voting.py

---
+++ voting.py
@@ -0,0 +1,25 @@
+# e = int(input("Enter your Date of birth:"))
+
+# if e>=18:
+#     print("You are eligible for voting")
+# elif e<18:
+#     print("You are not eligible for voting")
+
+
+# d = 2025
+# s = int(input("Enter your birth of year"))
+# r = d - s
+# if r>=18:
+#     print("You are eligible for voting")
+# elif r<18:
+#     print("You are not eligible for voting")
+
+# a={1,2,3,3}
+# print(a)
+
+# for i in range(1,1):
+#     print(i)
+
+# print(2**3**2)
+
+print(0.1+0.2)
